58c0d5cac6c7995480a47a3bd5b7a1f0de496edc
tinhlv44 <98213200+tinhlv44@users.noreply.github.com>
2025-06-04 21:13:21 +0700

    Revert "test"
    This reverts commit 1d39f70c731343bbf41941f1c7d19c73955c1e74.

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Weather App Demoo',
+      title: 'Weather App Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
